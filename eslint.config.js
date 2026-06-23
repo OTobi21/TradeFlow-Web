b2124dec045cb6dd8b5941d772042b7df654d793
NoobFaris <Ibrahimabdulganiy340@gmail.com>
2026-06-24 00:14:22 +0100

    fix: resolve ESLint 9 flat config conflict by removing legacy .eslintrc.json

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,37 @@
-const { FlatCompat } = require("@eslint/eslintrc");
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-module.exports = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
-    plugins: ["@typescript-eslint"],
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-      "react/no-unescaped-entities": "warn",
-      "react-hooks/exhaustive-deps": "warn",
+export default [
+  js.configs.recommended,
+  {
+    ignores: ['.next/**', 'node_modules/**', 'dist/**', 'out/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
     },
-  }),
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      ...ts.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
+      'react/no-unescaped-entities': 'warn',
+    },
+  },
 ];
